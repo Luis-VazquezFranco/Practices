4610d7fc08548130c970ee7d7bab0063a9e98219
Luis Vázquez Franco <110718257+Luis-VazquezFranco@users.noreply.github.com>
2025-08-11 21:49:02 +0200

    Time: 0 ms (100.00%) | Memory: 20.5 MB (60.00%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.php
@@ -0,0 +1,23 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function climbStairs(int $n): int {
+        if ($n <= 2) {
+            return $n;
+        }
+
+        $first = 1;
+        $second = 2;
+
+        for ($i = 3; $i <= $n; $i++) {
+            $third = $first + $second;
+            $first = $second;
+            $second = $third;
+        }
+
+        return $second;
+    }
+}
